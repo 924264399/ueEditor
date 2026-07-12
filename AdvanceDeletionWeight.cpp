06e145045ed1dc1bea51d5cff357d5084e05861c
刘耀宇 <924264399@qq.com>
2026-07-12 09:18:20 +0800

    0712

---
+++ AdvanceDeletionWeight.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SlateWights/AdvanceDeletionWeight.h"
+
+
+void ASdvanceDeletionTab::Construct(const FArguments& InArgs)
+{
+	bCanSupportFocus = true;  //是 SWidget 基类里的一个布尔成员变量，表示这个控件能不能获得键盘焦点（ 即能接受键盘输入  激活些什么）
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ AdvanceDeletionWeight.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+
+#include "Widgets/SCompoundWidget.h" //slat必须的类
+
+class ASdvanceDeletionTab : public SCompoundWidget   //是 Slate 所有 "复合控件" 的基类，你写自定义 Slate 控件基本都继承它
+{
+	
+	SLATE_BEGIN_ARGS(ASdvanceDeletionTab){ }
+
+		
+	SLATE_END_ARGS()   //这是一对宏  中间夹着的是参数声明，它们合起来会自动生成一个叫 FArguments 的内部结构体   简单说：这对宏 = 定义这个控件支持哪些外部参数
+                        // 编译器看就是 struct FArguments{     }
+						//以后可以用   SLATE_ATTRIBUTE 在中间声明参数
+	
+	
+public:
+	
+	void Construct(const FArguments& InArgs);  //这可以理解为构造函数  
+											   //const FArguments& InArgs 中的FArguments 就是上面的结构体来着		
+	
+	
+	
+	
+};
